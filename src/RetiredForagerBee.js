c1942c513e78d0e2e919aa83fd46fb363922d7cf
Arielle <ariellescherr@gmail.com>
2020-08-24 12:07:45 -0500

    Assignment Complete

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,18 @@
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends Bee {
+  constructor (age) {
+    super(age);
+    this.age = 40;
+    this.job = 'gamble';
+    this.canFly = false;
+    this.color = 'grey';
+    this.treasureChest = [];
+  }
+
+  forage () {
+    return "I am too old, let me play cards instead";
+  }
+
+  gamble () {
+    this.treasureChest.push('treasure');
+  }
 };
